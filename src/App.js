9aa8bfa6209896e7ccabdbfa3557ae2f6379cdaf
imran pasha I <imran.pi@dollarbirdinc.com>
2022-05-19 14:59:08 +0530

    adding projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Images } from './Images';
 
 function App() {
+  let img = Object.values(Images)
+  console.log();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="head-wrapper">
+        <h3 className="local-img-head">Local Images</h3>
+      </div>
+      <div className="local-images">
+        {img.map(item => {
+          return (
+            <div className="image_wrapper">
+              <img className="local-image" src={item} alt="image001" />
+            </div>
+          )
+        })}
+
+      </div>
+      <div className="head-wrapper">
+        <h3 className="local-img-head">Server Images</h3>
+      </div>
+      <div className="local-images">
+        <div className="image_wrapper">
+          <img className="local-image" src={"https://random.imagecdn.app/501/150"} alt="image001" />
+        </div>
+        <div className="image_wrapper">
+          <img className="local-image" src={"https://random.imagecdn.app/500/151"} alt="image001" />
+        </div>
+        <div className="image_wrapper">
+          <img className="local-image" src={"https://random.imagecdn.app/2000/150"} alt="image001" />
+        </div>
+        <div className="image_wrapper">
+          <img className="local-image" src={"https://random.imagecdn.app/2500/1150"} alt="image001" />
+        </div>
+        <div className="image_wrapper">
+          <img className="local-image" src={"https://random.imagecdn.app/5100/1250"} alt="image001" />
+        </div>
+      </div>
+      {/* <img className="local-image" src={"https://random.imagecdn.app/500/150"} alt="image001" /> */}
     </div>
   );
 }
---
+++ src/Images/index.js
@@ -0,0 +1,14 @@
+import image001 from './image001.jpg'
+import image002 from './image002.jpg'
+import image003 from './image003.jpg'
+import image004 from './image004.jpg'
+import image005 from './image005.jpg'
+
+
+export const Images = {
+    image001: image001,
+    image002: image002,
+    image003: image003,
+    image004: image004,
+    image005: image005,
+}
